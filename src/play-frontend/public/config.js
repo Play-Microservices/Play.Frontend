e3636dfca3b6f957471acf1066f41838222b770f
Furrman <furmanek.hubert@gmail.com>
2023-03-07 19:16:17 +0000

    Add const URLs to microservices

---
+++ src/play-frontend/public/config.js
@@ -0,0 +1,5 @@
+window.CATALOG_SERVICE_URL = `http://localhost:5234`; // Update URL to your Catalog service
+window.CATALOG_ITEMS_API_URL = `${window.CATALOG_SERVICE_URL}/items`;
+window.INVENTORY_SERVICE_URL = `http://localhost:5297`; // Update URL to your Inventory service
+window.INVENTORY_ITEMS_API_URL = `${window.INVENTORY_SERVICE_URL}/items`;
+window.RABBITMQ_URL = `http://localhost:15672`;
