3743eb492f1642f753fe851bb74ec7d954074275
Furmi <16333282+Furrman@users.noreply.github.com>
2026-07-12 14:56:31 +0100

    Update project to support Store

--- public/config.js
+++ public/config.js
@@ -7,10 +7,13 @@
 // Warning: this file will not be transpiled by Babel and cannot contain
 // any syntax that is not yet supported by your targeted browsers.
 
-window.CATALOG_SERVICE_URL = 'https://localhost:7105'
+window.CATALOG_SERVICE_URL = 'https://localhost:5001'
 window.CATALOG_ITEMS_API_URL = `${window.CATALOG_SERVICE_URL}/items`
 window.INVENTORY_SERVICE_URL = 'https://localhost:5005'
 window.INVENTORY_ITEMS_API_URL = `${window.INVENTORY_SERVICE_URL}/items`
 window.IDENTITY_SERVICE_URL = 'https://localhost:5003'
 window.USERS_API_URL = `${window.IDENTITY_SERVICE_URL}/users`
+window.TRADING_SERVICE_URL = 'https://localhost:5007'
+window.PURCHASE_API_URL = `${window.TRADING_SERVICE_URL}/purchase`
+window.STORE_API_URL = `${window.TRADING_SERVICE_URL}/store`
 window.RABBITMQ_URL = 'http://localhost:15672'
